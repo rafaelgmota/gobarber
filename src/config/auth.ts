e317f72978259f509ea5627fa5195d991aa37ea0
Rafael Mota <gmotarafael@gmail.com>
2020-05-09 17:57:43 -0300

    Add authenticantion

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9c194057fe74a5098212ee7e661eaa14',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import UsersRouter from './users.routes';
+import SessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', UsersRouter);
+routes.use('/sessions', SessionsRouter);
 
 export default routes;
